e962804d70d1603a0cae92558c61012b64d7127f
Sergio Garcia <gsergio488@gmail.com>
2020-12-18 22:30:36 -0500

    First commit
    Image conversion function and tests completed.

---
+++ change_image.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image
+
+
+RESOLUTION = (600, 400)
+
+
+def process_images(img_dir):
+    for infile in os.listdir(img_dir):
+        name, _ = os.path.splitext(infile)
+        abspath = os.path.join(img_dir, infile)
+        ofile = os.path.join(img_dir, name + '.JPEG')
+        try:
+            img = Image.open(os.path.join(img_dir, abspath))
+            img.convert('RGB').resize(RESOLUTION).save(ofile)
+        except FileNotFoundError as e:
+            print(f"The file {infile} could not be opened.")
+            raise e
+        except OSError as e:
+            print(f"Unable to save {infile}.")
+            raise e
+
+
+if __name__ == '__main__':
+    IMG_DIR = os.path.join(os.path.expanduser("~"), 'supplier-data', 'images')
+    process_images(IMG_DIR)
---
+++ tests/unit/test_change_image.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+import PIL
+
+from change_image import RESOLUTION, process_images
+
+
+class TestProcessImages(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = test_dir = os.path.join(os.environ.get("TEMP"), 'test_images')
+        if os.path.exists(self.test_dir):
+            for file in os.listdir(self.test_dir):
+                os.remove(os.path.join(self.test_dir, file))
+            os.rmdir(self.test_dir)
+        os.mkdir(self.test_dir)
+
+        # Save a test image.
+        self.test_fp = os.path.join(self.test_dir, 'test_image')
+        test_dimensions = (1000, 1000)
+        PIL.Image.new('RGBA', test_dimensions).save(os.path.join(self.test_fp + '.png'))
+
+    def test_process_images(self):
+        expected = (RESOLUTION, 'JPEG')
+        process_images(self.test_dir)
+        test_image = PIL.Image.open(self.test_fp + '.jpeg')
+        self.assertEqual(expected, (test_image.size, test_image.format))
